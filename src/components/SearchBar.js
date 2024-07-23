84e064c9ac3f43e03cc8877b469955fe74e4128f
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-07-23 12:38:38 +0200

    fixed small bug

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
