391169e6dde9c156a0e7c8b7bb629ccbd1c00497
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-07-24 20:38:00 +0200

    code minimalization try 11 - with add another path to RouterConfig

--- src/router/RouterConfig.js
+++ src/router/RouterConfig.js
@@ -2,18 +2,18 @@ import { Routes, Route } from "react-router-dom";
 
 import GamesList from "../components/GamesList";
 // import GameDetails from "../components/GameDetails";
-// import SearchResults from "../components/SearchResults";
-// import Error from "../components/Error";
+import SearchResults from "../components/SearchResults";
+import Error from "../components/Error";
 
 export default function RouterConfig() {
   return (
     <>
       <Routes>
         <Route path="/" element={<GamesList />} />
-        {/* <Route path="/search" element={<SearchResults />} /> */}
+        <Route path="/search" element={<SearchResults />} />
         {/* <Route path="/game/:id" element={<GameDetails />} /> */}
-        {/* <Route path="/error" element={<Error />} /> */}
-        {/* <Route path="/:listName" element={<GamesList />} /> */}
+        <Route path="/error" element={<Error />} />
+        <Route path="/:listName" element={<GamesList />} />
       </Routes>
     </>
   );
