eac579b91002da15d06a8cfc95c7cee2551a01de
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-07-24 16:53:45 +0200

    add modalInfo component

---
+++ src/components/ModalInfo.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getModalType } from "../store/selectors/selectors";
+
+export default function Info({ text }) {
+  const dispatch = useDispatch();
+  const modalType = useSelector(getModalType);
+
+  useEffect(() => {
+    const modalTimer = setTimeout(() => {
+      dispatch({ type: "HIDE_MODAL" });
+    }, 2250);
+
+    return () => clearTimeout(modalTimer);
+  }, []);
+
+  return (
+    <div className={`modal ${modalType.toLowerCase()}`}>
+      <p>{text}</p>
+    </div>
+  );
+}
