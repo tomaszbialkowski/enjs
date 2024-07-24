bbafba261018b0b84bdbd1c2c544ea983f741f9b
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-07-24 15:58:41 +0200

    add action to searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,21 @@
 import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
 
 const SearchBar = () => {
   const [query, setQuery] = useState("");
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const handleSearch = (e) => {
-  //   e.preventDefault();
-  //   navigate(`/search?q=${query}`);
-  //   setQuery("");
-  // };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/search?q=${query}`);
+    setQuery("");
+  };
 
   return (
-    // <form onSubmit={handleSearch} className="searchBar">
-    <form className="searchBar">
+    <form onSubmit={handleSearch} className="searchBar">
       <div className="searchWraper">
         <input
           type="text"
