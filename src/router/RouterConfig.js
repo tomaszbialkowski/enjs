188b36a3e710473b10f2df14855cc533d93445ed
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-07-24 17:06:48 +0200

    second try with modified RouterConfig

--- src/router/RouterConfig.js
+++ src/router/RouterConfig.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
-import GamesList from "../components/GamesList";
-import GameDetails from "../components/GameDetails";
+// import GamesList from "../components/GamesList";
+// import GameDetails from "../components/GameDetails";
 import SearchResults from "../components/SearchResults";
 import Error from "../components/Error";
 
@@ -9,11 +9,11 @@ export default function RouterConfig() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<GamesList />} />
+        {/* <Route path="/" element={<GamesList />} /> */}
         <Route path="/search" element={<SearchResults />} />
-        <Route path="/game/:id" element={<GameDetails />} />
+        {/* <Route path="/game/:id" element={<GameDetails />} /> */}
         <Route path="/error" element={<Error />} />
-        <Route path="/:listName" element={<GamesList />} />
+        {/* <Route path="/:listName" element={<GamesList />} /> */}
       </Routes>
     </>
   );
