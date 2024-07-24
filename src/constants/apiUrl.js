cd9d17e38fc5696200fcb03785d7041afd1c7db3
tomaszbialkowski <tomaszbialkowski78@gmail.com>
2024-07-24 20:52:16 +0200

    trying to fixed env

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,1 +1,2 @@
 export const API_URL_WITH_KEY = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+// export const API_URL_WITH_KEY = `http://www.omdbapi.com/?apikey=55ca2044`;
